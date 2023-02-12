e98d0be67ab756dc9b01dfa8e202722285476c2f
welliton gervickas <wellitogervickas@gmail.com>
2023-02-12 00:07:23 -0300

    chore: postcss setup

--- modules/tailwind/themes/default.js
+++ modules/tailwind/themes/default.js
@@ -5,6 +5,11 @@ module.exports = {
         DEFAULT: '#BEE719',
         100: '#E8FF89',
         900: '#8AA814'
+      },
+      grey: {
+        DEFAULT: '#8F8F8F',
+        100: '##343434',
+        900: '#343434'
       }
     }
   }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <main className='h-full w-full'>Swapper</main>
+      <main>Swapper</main>
     </>
   )
 }
