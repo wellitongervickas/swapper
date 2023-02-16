a51b010f13857c4b03ba14df923c5eab1500f066
welliton gervickas <wellitogervickas@gmail.com>
2023-02-16 14:17:54 -0300

    fix: adding my profile as discord

--- config/index.ts
+++ config/index.ts
@@ -5,6 +5,9 @@ const config = {
   meta: {
     description: 'Decentralized finance app to swap tokens'
   },
+  social: {
+    discordURL: 'https://discord.com/users/gervickas.js#0001'
+  },
   providers: {
     defaultChainId: chains.goerli.id
   },
