ffa02cf6e5a79a7b856432bd2b49c58433156ae0
welliton gervickas <wellitogervickas@gmail.com>
2023-02-16 13:43:00 -0300

    fix: remove logo asset

--- components/shared/Logo.tsx
+++ components/shared/Logo.tsx
@@ -2,8 +2,7 @@ import config from '@/config'
 
 const Logo = () => (
   <div className='flex flex-row items-center justify-center space-x-2 leading-none'>
-    <span className='text-xl font-black text-primary'>&#187;</span>
-    <span className=''>{config.name}</span>
+    <span className='text-2xl'>{config.name}</span>
   </div>
 )
 
