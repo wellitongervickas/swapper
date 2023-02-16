214826a2f171e91b44a5ade7f95a6a9600e5dd82
welliton gervickas <wellitogervickas@gmail.com>
2023-02-16 14:33:49 -0300

    fix: checkbox input event bubbles

--- components/shared/form/Checkbox.tsx
+++ components/shared/form/Checkbox.tsx
@@ -7,32 +7,30 @@ interface FormCheckboxProps extends ComponentProps<'input'> {
 
 const FormCheckbox = ({ label, ...props }: FormCheckboxProps) => {
   return (
-    <div className='flex flex-row items-center space-x-2'>
+    <label htmlFor={props.id} className='cursor-pointer'>
       <input type='checkbox' {...props} hidden />
-      <div
-        className={classnames.merge([
-          'relative h-5 w-5 rounded-md bg-stone-900'
-        ])}
-      >
-        {props.checked ? (
-          <div
-            className={classnames.merge([
-              'absolute flex h-full w-full items-center ',
-              'justify-center text-xs text-primary'
-            ])}
-          >
-            <span>&#10003;</span>
-          </div>
-        ) : (
-          <div></div>
-        )}
+      <div className='flex flex-row items-center space-x-2'>
+        <div
+          className={classnames.merge([
+            'relative h-5 w-5 rounded-md bg-stone-900'
+          ])}
+        >
+          {props.checked ? (
+            <div
+              className={classnames.merge([
+                'absolute flex h-full w-full items-center ',
+                'justify-center text-xs text-primary'
+              ])}
+            >
+              <span>&#10003;</span>
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        {label && <span className='leading-none'>{label}</span>}
       </div>
-      {label && (
-        <label htmlFor={props.id} className='cursor-pointer leading-none'>
-          {label}
-        </label>
-      )}
-    </div>
+    </label>
   )
 }
 
